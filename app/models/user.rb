9aa4409eb86619f080082b280cd1eec1165a5efd
Kyle Rego <Kyle.Rego@gmail.com>
2016-04-11 16:35:26 -0700

    removed ability to sign up without omniauth, updated routes, removed view code, potential bug at current checkpoint: 1st click on signinwithspotify does not sign in; subsequent clicks signs user in properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   	# Include default devise modules. Others available are:
   	# :confirmable, :lockable, :timeoutable and :omniauthable
-  	devise :database_authenticatable, :registerable, :omniauthable,
+  	devise :database_authenticatable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   	def password_required?
   		super && provider.blank?
   	end
+
+  	def update_with_password(params, *options)
+  		if encrypted_password.blank?
+  			update_attributes(params, *options)
+  		else
+  			super
+  		end
+  	end
+  	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
+   as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    delete 'users' => 'devise/registrations#destroy', :via => :delete, :as => 'cancel_registration'
+  end
+
   root 'home#index'
-  #get 'users/auth/spotify/callback/', to: 'callbacks#spotify'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
