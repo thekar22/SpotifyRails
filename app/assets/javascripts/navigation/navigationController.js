8c87fa67740fdf6f5625d6d24cf2a4d9fe5c5471
Kyle Rego <Kyle.Rego@gmail.com>
2016-08-12 12:54:26 -0700

    CSS changes; added logic to only show get audio features if on tag view

--- app/assets/javascripts/navigation/navigationController.js
+++ app/assets/javascripts/navigation/navigationController.js
@@ -1,10 +1,22 @@
 angular
 	.module('navigationModule', [])
-	.controller('navigationController', ['$scope', '$mdSidenav', function tagController($scope, $mdSidenav) {
+	.controller('navigationController', ['$scope', '$mdSidenav', '$location', function tagController($scope, $mdSidenav, $location) {
+		
+		$scope.$on('$locationChangeStart', function(event) {
+			if ($location.path() !== "/")
+			{
+				$scope.navView = "other-view";
+			}
+			else
+			{
+				$scope.navView = "tag-view";
+			}
+		});
+
 		$scope.openLeftMenu = function() {
 			$mdSidenav('left').toggle();
 		};	
 			$scope.openRightMenu = function() {
 			$mdSidenav('right').toggle();
-		};	
+		};
 }]);
